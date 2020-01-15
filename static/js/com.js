3c3407be78beea547987e5d20571498033cc16af
wendy <2313106773@qq.com>
2020-01-15 18:05:38 +0800

    '考试逻辑'

--- static/js/com.js
+++ static/js/com.js
@@ -10,7 +10,7 @@ var Com = (function Module(){
     return null;
   }
   // 网络请求
-  var ajax = function(url, params, callback0, callback1){
+  var get = function(url, params, callback0, callback1){
     console.log(url, params)
     axios.get(url, {
       params: params
@@ -21,7 +21,26 @@ var Com = (function Module(){
         if(rs.data.code === 0){
           callback0(rs.data.data)
         } else if(callback1){
-          callback1(rs.data.data)  
+          callback1(rs.data)  
+        }
+    })
+    .catch(function(error){
+      console.log('接口出错：' + url);
+      console.log(error);
+    })
+  }
+  var post = function(url, params, callback0, callback1){
+    console.log(url, params)
+    axios.post(url, {
+      params: params
+    })
+    .then(function(rs) {
+        console.log(rs)
+        // code为0处理一般成功情况
+        if(rs.data.code === 0){
+          callback0(rs.data.data)
+        } else if(callback1){
+          callback1(rs.data)  
         }
     })
     .catch(function(error){
@@ -32,7 +51,8 @@ var Com = (function Module(){
   var module = {
     goback: goback,
     getParams: getParams,
-    ajax: ajax
+    get: get,
+    post: post
   }
   return module
 }())
